5be2dd0ffb0058ae893510ff00e451ada3ec9ab8
Brad Meyn <bradjmeyn@gmail.com>
2021-06-06 22:13:28 +1000

    add route to view all investments with ejs view engine

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+
+const Investment = require('./models/investment');
+
+
+//connect mongoose to mongodb server
+mongoose.connect('mongodb://localhost:27017/investmentAPI', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+//confirm database connection
+const db = mongoose.connection;
+db.on('error',console.error.bind('connection error:'));
+db.once('open', () => {
+    console.log("database connected");
+});
+
 const app = express();
 
-
+//set the view ehgine to use ejs
+app.set('view engine', 'ejs');
+//views directory location
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.send("hey")
@@ -10,10 +33,9 @@ app.get('/', (req, res) => {
 
 //display all investments
 app.get('/investments', async (req, res) => {
-    res.send("all investments")
+    const investments = await Investment.find();
+    res.render("investments/all", {investments})
 });
-
-
 
 
 app.listen(3000, ()=> {
