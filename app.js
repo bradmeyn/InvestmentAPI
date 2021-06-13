b726ef310fd1dc68ee45a63951ddb6bc22ec1138
Brad Meyn <bradjmeyn@gmail.com>
2021-06-13 18:43:25 +1000

    add ability to update and handle percentages in form

--- app.js
+++ app.js
@@ -56,11 +56,18 @@ app.get('/investments/add', (req, res) =>{
 
 //post route for new investment
 app.post('/investments', async (req, res)=> {
-    const investment = new Investment(req.body.investment)
 
-    await investment.save();
+    const investment = {...req.body.investment};
+    investment.cost = parseFloat(investment.cost)/100;
+    console.log(investment);
+    const newInvestment = new Investment(investment);
+    
+    await newInvestment.save();
     // const investment = new Investment(req)
+
+    console.log(`${investment.name} created`);
     res.redirect(`/investments/${investment.code}`)
+
 })
 
 //display single investment
@@ -71,17 +78,33 @@ app.get('/investments/:code', async (req, res) => {
         res.render('investments/show', {investment});
     });
 
+//delete single investment
 app.delete('/investments/:code', (req, res)=>{
-    console.log(req.params.code);
-    res.send('delete');
+    Investment.findOneAndDelete({code: req.params.code},()=>{
+        console.log(`${req.params.code} deleted`);
+    });
+    res.redirect('/investments')
 });
 
-app.get('/investments/:code/edit', (req, res)=>{
-    res.send("edit")
+
+//show update page
+app.get('/investments/:code/update', async (req, res) =>{
+    const {code} = req.params;
+    const investment = await Investment.findOne({code: code});
+    
+    res.render(`investments/update`, {investment,numeral});
 });
 
-app.put('/investments/:code', (req, res)=>{
-    res.send('edit')
+//put address for update form submission
+app.put('/investments/:code',async (req, res)=>{
+    const {code} = req.params;
+    const investment = {...req.body.investment};
+    investment.cost = parseFloat(investment.cost)/100;
+    await Investment.findOneAndUpdate({code: code}, {...investment});
+    console.log(`${investment.name} updated`);
+    
+
+    res.redirect('/investments')
 });
 
 
